181754c676d6f52572beaf58141117c5527edf8b
Colin Breese <ib2member01@gmail.com>
2017-10-11 13:48:35 -0400

    Added userAgent to avoid android error

--- fragments/rtcheck.js
+++ fragments/rtcheck.js
@@ -4,4 +4,4 @@
 /*
  * Add GNU GPL text here
 */
-window.onload=function(){var platform=window.navigator.platform,os=ib2f.config.runtime.os;if(/Android/.test(userAgent)){os="Android";}else if(!os && /Linux/.test(platform)){os="Linux";}else if(!os && /ChrOS/.test(platform)){os="ChromeOS";}else if(!os && /2Term/.test(platform)){os="2Term/IB2OS";}else{os=window.navigator.platform;}}
+window.onload=function(){var platform=window.navigator.platform,os=ib2f.config.runtime.os,userAgent=window.navigator.userAgent;if(/Android/.test(userAgent)){os="Android";}else if(!os && /Linux/.test(platform)){os="Linux";}else if(!os && /ChrOS/.test(platform)){os="ChromeOS";}else if(!os && /2Term/.test(platform)){os="2Term/IB2OS";}else{os=window.navigator.platform;}}
